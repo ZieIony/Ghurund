2b58565f3eeccba54c185f72a191ee179ad684e7
Zielony <niewartotupisac@gmail.com>
2021-05-24 00:06:06 +0200

    added Range and unit tests for Range

---
+++ engine/Engine.Core/src/core/collection/Range.h
@@ -0,0 +1,71 @@
+#pragma once
+
+namespace Ghurund {
+    template<typename Type>
+    class Range {
+    private:
+        class iterator {
+        private:
+            Type i, b, step;
+            int sign;
+
+        public:
+            iterator(Type i, Type b, int sign, Type step):i(i), b(b), sign(sign), step(step) {}
+
+            inline Type operator*() const {
+                return b + sign * i * step;
+            }
+
+            inline const iterator& operator++() {
+                i++;
+                return *this;
+            }
+
+            inline iterator operator++(int) {
+                iterator copy(*this);
+                i++;
+                return copy;
+            }
+
+            inline bool operator==(const iterator& other) const {
+                return i == other.i && b == other.b && sign == other.sign && step == other.step;
+            }
+        };
+
+        Type b, step, count;
+        int sign;
+
+        inline Type getRange(Type begin, Type end) {
+            return begin < end ? end - begin : begin - end;
+        }
+
+    public:
+        Range(Type begin, Type end):b(begin), step(1) {
+            sign = end > begin ? 1 : -1;
+            count = getRange(begin, end);
+        }
+
+        Range(Type begin, Type end, Type step):b(begin), step(step) {
+            if (step <= 0 || getRange(begin, end) < getRange(0, step))
+                throw std::invalid_argument("step has to be > 0 and < abs(end-begin)");
+            sign = end > begin ? 1 : -1;
+            count = getRange(begin, end) / step;
+        }
+
+        inline iterator begin() {
+            return iterator(0, b, sign, step);
+        }
+
+        inline iterator begin() const {
+            return iterator(0, b, sign, step);
+        }
+
+        inline iterator end() {
+            return iterator(count + 1, b, sign, step);
+        }
+
+        inline iterator end() const {
+            return iterator(count + 1, b, sign, step);
+        }
+    };
+}
---
+++ engine/UnitTest/src/core/collection/RangeTest.cpp
@@ -0,0 +1,114 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "core/collection/Range.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest {
+    using namespace Ghurund;
+    using namespace std;
+
+    TEST_CLASS(RangeTest) {
+public:
+
+    TEST_METHOD(Range_inc) {
+        auto range = Range<size_t>(0, 3);
+        auto iterator = range.begin();
+        Assert::AreEqual(0ull, *iterator);
+        iterator++;
+        Assert::AreEqual(1ull, *iterator);
+        iterator++;
+        Assert::AreEqual(2ull, *iterator);
+        iterator++;
+        Assert::AreEqual(3ull, *iterator);
+        iterator++;
+        Assert::AreEqual(4ull, *iterator);
+        Assert::IsTrue(range.end() == iterator);
+    }
+
+    TEST_METHOD(Range_inc_float) {
+        auto range = Range<float>(0, 1, 0.5f);
+        auto iterator = range.begin();
+        Assert::AreEqual(0.0f, *iterator);
+        iterator++;
+        Assert::AreEqual(0.5f, *iterator);
+        iterator++;
+        Assert::AreEqual(1.0f, *iterator);
+        iterator++;
+        Assert::AreEqual(1.5f, *iterator);
+        Assert::IsTrue(range.end() == iterator);
+    }
+
+    TEST_METHOD(Range_inc_step) {
+        auto range = Range<size_t>(0, 3, 2);
+        auto iterator = range.begin();
+        Assert::AreEqual(0ull, *iterator);
+        iterator++;
+        Assert::AreEqual(2ull, *iterator);
+        iterator++;
+        Assert::AreEqual(4ull, *iterator);
+        Assert::IsTrue(range.end() == iterator);
+    }
+
+    TEST_METHOD(Range_dec) {
+        auto range = Range<int>(3, 0, 1);
+        auto iterator = range.begin();
+        Assert::AreEqual(3, *iterator);
+        iterator++;
+        Assert::AreEqual(2, *iterator);
+        iterator++;
+        Assert::AreEqual(1, *iterator);
+        iterator++;
+        Assert::AreEqual(0, *iterator);
+        iterator++;
+        Assert::AreEqual(-1, *iterator);
+        Assert::IsTrue(range.end() == iterator);
+    }
+
+    TEST_METHOD(Range_dec_unsigned) {
+        auto range = Range<size_t>(3, 0, 1);
+        auto iterator = range.begin();
+        Assert::AreEqual(3ull, *iterator);
+        iterator++;
+        Assert::AreEqual(2ull, *iterator);
+        iterator++;
+        Assert::AreEqual(1ull, *iterator);
+        iterator++;
+        Assert::AreEqual(0ull, *iterator);
+        iterator++;
+        Assert::AreEqual((size_t)-1, *iterator);
+        Assert::IsTrue(range.end() == iterator);
+    }
+
+    TEST_METHOD(Range_dec_autoStep) {
+        auto range = Range<int>(3, 0);
+        auto iterator = range.begin();
+        Assert::AreEqual(3, *iterator);
+        iterator++;
+        Assert::AreEqual(2, *iterator);
+        iterator++;
+        Assert::AreEqual(1, *iterator);
+        iterator++;
+        Assert::AreEqual(0, *iterator);
+        iterator++;
+        Assert::AreEqual(-1, *iterator);
+        Assert::IsTrue(range.end() == iterator);
+    }
+
+    TEST_METHOD(Range_bigStep) {
+        auto func = [] { Range<int>(3, 0, 5); };
+        Assert::ExpectException<std::invalid_argument>(func);
+    }
+
+    TEST_METHOD(Range_negativeStep) {
+        auto func = [] { Range<float>(3, 0, -1.0f); };
+        Assert::ExpectException<std::invalid_argument>(func);
+    }
+
+    TEST_METHOD(Range_zeroStep) {
+        auto func = [] { Range<int>(3, 0, 0); };
+        Assert::ExpectException<std::invalid_argument>(func);
+    }
+    };
+}
